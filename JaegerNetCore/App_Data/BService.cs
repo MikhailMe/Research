7cdd7cc409e9abed1df570105c09fb15fdc589ee
MikhailMe <neon-pf@yandex.ru>
2019-01-10 16:34:45 +0300

    removed consul

--- JaegerNetCore/App_Data/BService.cs
+++ JaegerNetCore/App_Data/BService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using Consul;
 using Dapper;
 
 namespace JaegerNetCoreSecond.App_Data
@@ -24,29 +23,7 @@ namespace JaegerNetCoreSecond.App_Data
                 var dbRes = (await db.QueryAsync<string>(command)).ToList();
                 var res = _webClient.DownloadString(url);
                 dbRes.Add(res);
-                HelloConsul().GetAwaiter().GetResult();
                 return dbRes.ToArray();
-            }
-        }
-
-        public static async Task<string> HelloConsul()
-        {
-            using (var client = new ConsulClient())
-            {
-                var putPair = new KVPair("hello")
-                {
-                    Value = Encoding.UTF8.GetBytes("Hello Consul")
-                };
-
-                var putAttempt = await client.KV.Put(putPair);
-
-                if (putAttempt.Response)
-                {
-                    var getPair = await client.KV.Get("hello");
-                    return Encoding.UTF8.GetString(getPair.Response.Value, 0,
-                        getPair.Response.Value.Length);
-                }
-                return "";
             }
         }
 
